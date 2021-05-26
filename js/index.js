207ed214ec03930c96b1655b2ea37a03b0671313
ss <72294816+sas-28@users.noreply.github.com>
2021-05-26 23:26:18 +0530

    complete swiper portfolio

--- js/index.js
+++ js/index.js
@@ -135,3 +135,21 @@ modalCloses.forEach((modalClose) => {
     })
   })
 })
+
+/* -------------- Swiper --------------- */
+
+let swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // mousewheel: true,
+  // keyboard: true,
+});
