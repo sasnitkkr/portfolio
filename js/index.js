7dafae17ef743066b7e0ace46e41c69024a388bd
ss <72294816+sas-28@users.noreply.github.com>
2021-05-26 17:54:05 +0530

    Completed Services

--- js/index.js
+++ js/index.js
@@ -85,3 +85,53 @@ function toggleSkills(){
 
 skillsHeader.forEach(n => n.addEventListener('click', toggleSkills));
 
+
+/* -------------- Qualification Tabs --------------- */
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabsContents = document.querySelectorAll('[data-content]');
+    
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () => {
+
+      const target = document.querySelector(tab.dataset.target)
+
+      tabsContents.forEach(tabsContent =>{
+        tabsContent.classList.remove('qualification__active');
+      })
+      target.classList.add('qualification__active');
+
+      tabs.forEach(tab => {
+        tab.classList.remove('qualification__active');
+      })
+      tab.classList.add('qualification__active');
+
+    })  
+})
+    
+
+/* -------------- Services Modal --------------- */
+
+const modalViews = document.querySelectorAll('.services__modal'), // outside modal box
+      modalBtns = document.querySelectorAll('.services__button'),  // view more button
+      modalCloses = document.querySelectorAll('.services__modal-close'); // all cross
+
+      // remember to give . in querySelectorAll class
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
